cc558fd50bcce1fb798f5711f7ffb8e593d1a2cc
RaulEstevezA <tevezabella@hotmail.com>
2025-01-30 01:53:49 +0100

    nueva pagina

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // Abrir enlaces en una nueva ventana
     var links = document.querySelectorAll('a');
     links.forEach(function(link) {
-        link.setAttribute('target', '_blank');
+        const href = link.getAttribute('href');
+        if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+            link.setAttribute('target', '_blank');
+        }
     });
 
     // Funcionalidad para la primera tabla
@@ -32,6 +35,50 @@ document.addEventListener("DOMContentLoaded", function () {
             toggleButton2.textContent = 'Show';
         }
     });
+
+    // Funcionalidad para la pantalla inicial
+    const introScreen = document.getElementById('intro-screen');
+    const header = document.querySelector('.navbar'); // Seleccionamos la barra superior
+    let introHidden = false; // Variable para registrar si la pantalla inicial ya fue oculta
+
+    // Asegurarnos de que la barra esté oculta al principio
+    header.style.opacity = 0;
+    header.style.pointerEvents = "none"; // Desactiva interacción al principio
+
+    // Detectar el scroll para ocultar la pantalla inicial y mostrar la barra superior
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50 && !introHidden) {
+            introScreen.classList.add('hidden'); // Ocultar pantalla inicial
+            header.style.opacity = 1; // Mostrar la barra superior
+            header.style.pointerEvents = "auto"; // Activar interacción con la barra
+            introHidden = true; // Marcar que la pantalla inicial ya fue ocultada
+        } else if (window.scrollY <= 50 && !introHidden) {
+            introScreen.classList.remove('hidden'); // Mostrar la pantalla inicial si no ha sido ocultada definitivamente
+            header.style.opacity = 0; // Ocultar la barra superior
+            header.style.pointerEvents = "none"; // Desactivar interacción
+        }
+    });
+
+
+
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Evita el comportamiento predeterminado
+            const targetId = this.getAttribute('href').substring(1); // Obtén el ID de destino
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const offset = 120; // Altura de la barra superior
+                const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+                const offsetPosition = elementPosition - offset;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: 'smooth' // Desplazamiento suave
+                });
+            }
+        });
+    });
+
+
 });
-
-
